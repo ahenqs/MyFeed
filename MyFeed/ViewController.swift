8a227e095f725281b049133a60b3e78c99153f2d
André Henrique da Silva <ahenqs@gmail.com>
2016-07-22 11:33:43 -0300

    Initial Commit

---
+++ MyFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFeed
+//
+//  Created by André Henrique da Silva on 22/07/2016.
+//  Copyright © 2016 André Henrique da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
